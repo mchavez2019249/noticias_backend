15670e16b2bec960b734c3387b39b131dc1e0d3f
Marlon Chávez <mchavez-2019249@kinal.edu.gt>
2025-01-13 11:43:06 -0600

    first commit

---
+++ src/main/java/com/mchavez/Noticias_BE/NoticiasBeApplication.java
@@ -0,0 +1,13 @@
+package com.mchavez.Noticias_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticiasBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticiasBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mchavez/Noticias_BE/NoticiasBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchavez.Noticias_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticiasBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
